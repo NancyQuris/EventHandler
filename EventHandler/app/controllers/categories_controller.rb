81928e0e340fc42167fba513315ffc1d286990f0
NancyQuris <liuyingnan1999@gmail.com>
2019-09-28 17:12:03 +0800

    fix bug in update and create API for category

--- EventHandler/app/controllers/categories_controller.rb
+++ EventHandler/app/controllers/categories_controller.rb
@@ -4,13 +4,13 @@ class CategoriesController < ActionController::Base
   def create
     category = request.params[:category]
     if Category.exists?(categoryName: category)
+      head 400 #bad request, the category name has been occupied
+    else
       new_category = Category.create(categoryName: category, 
         createdTime: DateTime.now, updatedTime: DateTime.now)
       json = {"categoryId" => new_category.id, 
               "category" => category}
       render :json => json
-    else
-      head 400 #bad request, the category name has been occupied
     end 
   end
 
@@ -41,14 +41,14 @@ class CategoriesController < ActionController::Base
     category_name = request.params[:category]
     if Category.exists?(category_id)
       if Category.exists?(categoryName: category_name)
+        head 400 #bad request, the category name has been occupied
+      else
         category = Category.find(category_id)
         category.categoryName = category_name
         category.updatedTime = DateTime.now
         json = {"categoryId" => category.id, 
           "category" => category_name}
         render :json => json
-      else
-        head 400 #bad request, the category name has been occupied
       end
     else
       head 500
