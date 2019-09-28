9fba2a24d04a28b6433f14a4220be41f655f9dea
NancyQuris <liuyingnan1999@gmail.com>
2019-09-28 17:05:27 +0800

    restrict category name to be distinct

--- EventHandler/app/controllers/categories_controller.rb
+++ EventHandler/app/controllers/categories_controller.rb
@@ -1,15 +1,17 @@
 class CategoriesController < ActionController::Base
-  skip_before_action :verify_authenticity_token
-  
-  #TO-DO: ensure distinction of category name 
+  skip_before_action :verify_authenticity_token 
 
   def create
     category = request.params[:category]
-    new_category = Category.create(categoryName: category, 
-      createdTime: DateTime.now, updatedTime: DateTime.now)
-    json = {"categoryId" => new_category.id, 
-            "category" => category}
-    render :json => json
+    if Category.exists?(categoryName: category)
+      new_category = Category.create(categoryName: category, 
+        createdTime: DateTime.now, updatedTime: DateTime.now)
+      json = {"categoryId" => new_category.id, 
+              "category" => category}
+      render :json => json
+    else
+      head 400 #bad request, the category name has been occupied
+    end 
   end
 
   def delete
@@ -38,12 +40,16 @@ class CategoriesController < ActionController::Base
     category_id = request.query_parameters[:categoryId]
     category_name = request.params[:category]
     if Category.exists?(category_id)
-      category = Category.find(category_id)
-      category.categoryName = category_name
-      category.updatedTime = DateTime.now
-      json = {"categoryId" => category.id, 
-        "category" => category_name}
-      render :json => json
+      if Category.exists?(categoryName: category_name)
+        category = Category.find(category_id)
+        category.categoryName = category_name
+        category.updatedTime = DateTime.now
+        json = {"categoryId" => category.id, 
+          "category" => category_name}
+        render :json => json
+      else
+        head 400 #bad request, the category name has been occupied
+      end
     else
       head 500
     end
